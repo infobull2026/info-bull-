b6a234e1e45f1eccc7a325406dc540258ad3ef49
infobull2026 <info.shreetwana2005@gmail.com>
2026-08-06 00:48:01 +0100

    Update market.js

--- scripts/market.js
+++ scripts/market.js
@@ -1,1 +1,33 @@
-const response = await fetch("https://info-bull.onrender.com/market");
+console.log("Market JS loaded");
+async function loadMarketData() {
+
+  try {
+
+    const response = await fetch("https://info-bull.onrender.com/market");
+
+    const data = await response.json();
+
+    document.getElementById("nepseIndex").innerHTML =
+    "Index Value: " + data.index;
+
+    document.getElementById("nepseChange").innerHTML =
+    "Change: " + data.change;
+
+    document.getElementById("turnover").innerHTML =
+    "Amount: " + data.turnover;
+
+    document.getElementById("marketStatus").innerHTML =
+    data.status;
+
+  } catch (error) {
+
+    document.getElementById("marketStatus").innerHTML =
+    "Unable to load market data.";
+
+    console.log(error);
+
+  }
+
+}
+
+loadMarketData();
