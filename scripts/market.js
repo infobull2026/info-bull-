2bfb00d4d3656c42ec7322907b2d2c6640ea5272
infobull2026 <info.shreetwana2005@gmail.com>
2026-08-05 23:31:49 +0100

    Add market data script

---
+++ scripts/market.js
@@ -0,0 +1,17 @@
+function loadMarketData() {
+
+  document.getElementById("nepseIndex").innerHTML =
+  "Index Value: Loading...";
+
+  document.getElementById("nepseChange").innerHTML =
+  "Change: Loading...";
+
+  document.getElementById("turnover").innerHTML =
+  "Amount: Loading...";
+
+  document.getElementById("marketStatus").innerHTML =
+  "Market data connection ready.";
+
+}
+
+loadMarketData();
