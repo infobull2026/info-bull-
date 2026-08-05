3b07498054e11d7439027420f8d48805055eb10a
infobull2026 <info.shreetwana2005@gmail.com>
2026-08-06 00:07:16 +0100

    Connect dashboard to backend API

--- scripts/market.js
+++ scripts/market.js
@@ -1,17 +1,1 @@
-function loadMarketData() {
-
-  document.getElementById("nepseIndex").innerHTML =
-  "Index Value: 2650.50";
-
-  document.getElementById("nepseChange").innerHTML =
-  "Change: +12.25 (+0.46%)";
-
-  document.getElementById("turnover").innerHTML =
-  "Amount: Rs. 5,000,000,000";
-
-  document.getElementById("marketStatus").innerHTML =
-  "Market Open";
-
-}
-
-loadMarketData();
+YOUR_BACKEND_URL
